5db8add3e7f0ae827f4e337953ac95e2fc9fb74e
Hamed Asghari <hasghari@g2.com>
2020-03-24 14:17:20 -0600

    Add configuration options for executor job

--- lib/one_off.rb
+++ lib/one_off.rb
@@ -1,8 +1,19 @@
 # frozen_string_literal: true
 
 require 'one_off/engine'
+require 'one_off/configuration'
 require 'one_off/file'
 
 module OneOff
-  # Your code goes here...
+  class << self
+    def configuration
+      @configuration ||= OneOff::Configuration.new
+    end
+
+    alias config configuration
+
+    def configure
+      yield configuration
+    end
+  end
 end
